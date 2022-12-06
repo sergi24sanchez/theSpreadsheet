c86f968860cb8d06f450e59a092da794efd30c52
Sergi Sánchez Bonilla <sergi24sanchez@gmail.com>
2022-12-06 15:43:42 +0100

    First classes created

---
+++ Spreadsheet.py
@@ -0,0 +1,11 @@
+class Spreadsheet():
+
+
+    def __init__(self):
+        self.cells = []
+    
+    def get_cells(self):
+        return self.cells
+    
+    def set_cells(self, cells):
+        self.cells = cells
