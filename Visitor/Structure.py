cadbb19ce27682ceef1ad0ac37f2e54fcc1eed42
Sergi Sánchez Bonilla <sergi24sanchez@gmail.com>
2023-01-23 10:01:05 +0100

    Deleted Visitor folder

--- Visitor/Structure.py
+++
@@ -1,9 +0,0 @@
-from Element import Element
-
-class Structure:
-
-    def __init__(self) -> None:
-        self.elements = []
-    
-    def add_element(self,element:Element):
-        self.elements.append(element)
